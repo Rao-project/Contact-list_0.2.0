1fc14ce984e92033d13163c75a533a99035d7ace
Divyesh-Sar2003 <119989934+Divyesh-Sar2003@users.noreply.github.com>
2023-08-05 17:56:47 +0530

    Redirect the links

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const sqlite3 = require("sqlite3").verbose();
 let sql;
 
 // connect to database
-const db = new sqlite3.Database("./contact.db", sqlite3.OPEN_READWRITE, (err) =>{
+const db = new sqlite3.Database("./data.db", sqlite3.OPEN_READWRITE, (err) =>{
     if (err) return console.error(err.message);
     console.log("Connected to database");
 });
 
-// Create table 
+// Create table
 
 // sql = 'CREATE TABLE contact(id INTEGER PRIMARY KEY,first_name,last_name,phone_number_1,label_1,phone_number_2,lable_2,email,birthdate DATE,note,image,is_favorite)';
 // db.run(sql , (err) =>{
@@ -18,25 +18,16 @@ const db = new sqlite3.Database("./contact.db", sqlite3.OPEN_READWRITE, (err) =>
 
 // Insert query
 
-// sql = 'INSERT INTO contact(first_name,last_name,phone_number_1,label_1,phone_number_2,lable_2,email,birthdate,note,image,is_favorite) VALUES ("Sarvaiya","Divyesh",6352239294,"work",9898899520,"home","test2003@gmail.com",2003-12-02,"Hello india","data: url",0)';
+// sql = 'INSERT INTO contact(first_name,last_name,phone_number_1,label_1,phone_number_2,lable_2,email,birthdate,note,image,is_favorite) VALUES ("Sarvaiya","Divyesh",6352239294,"work",9898899520,"home","test2003@gmail.com","02-Dec-2003","Hello india","Image:url",0)';
 // db.run(sql , (err , data) =>{
 //     if(err) {
 // 		return console.log(err.message); 
 // 	}
-// 	console.log('Row was added to the table');
+// 	console.log('Row was added to the table:');
 
 // });
 
-//delete data
-
-// sql = 'DELETE FROM contact WHERE id = 2';
-// db.run(sql ,(err) => {
-//     if(err) err.message;
-//     console.log("data deleted successfully");
-// });
-
-
-//select data
+// //select data
 
 // sql = 'SELECT * FROM contact';
 // db.all(sql, [], (err, rows) => {
@@ -47,7 +38,6 @@ const db = new sqlite3.Database("./contact.db", sqlite3.OPEN_READWRITE, (err) =>
 //       console.log(row);
 //     });
 //   });
-
 
 //Drop table
 
