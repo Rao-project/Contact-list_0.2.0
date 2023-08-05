b85269e304cdc8b05f4539c47e20d557f1993276
Divyesh-Sar2003 <119989934+Divyesh-Sar2003@users.noreply.github.com>
2023-08-05 15:09:50 +0530

    add query

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ const db = new sqlite3.Database("./contact.db", sqlite3.OPEN_READWRITE, (err) =>
 
 // Insert query
 
-sql = 'INSERT INTO contact(first_name,last_name,phone_number_1,label_1,phone_number_2,lable_2,email,birthdate,note,image,is_favorite) VALUES ("Sarvaiya","Divyesh",6352239294,"work",9898899520,"home","test2003@gmail.com",2003-12-02,"Hello india","data: url",0)';
-db.run(sql , (err , data) =>{
-    if(err) {
-		return console.log(err.message); 
-	}
-	console.log('Row was added to the table');
+// sql = 'INSERT INTO contact(first_name,last_name,phone_number_1,label_1,phone_number_2,lable_2,email,birthdate,note,image,is_favorite) VALUES ("Sarvaiya","Divyesh",6352239294,"work",9898899520,"home","test2003@gmail.com",2003-12-02,"Hello india","data: url",0)';
+// db.run(sql , (err , data) =>{
+//     if(err) {
+// 		return console.log(err.message); 
+// 	}
+// 	console.log('Row was added to the table');
 
-});
+// });
 
 //delete data
 
@@ -38,15 +38,21 @@ db.run(sql , (err , data) =>{
 
 //select data
 
-sql = 'SELECT * FROM contact';
-db.all(sql, [], (err, rows) => {
-    if (err) {
-      throw err;
-    }
-    rows.forEach((row) => {
-      console.log(row);
-    });
-  });
+// sql = 'SELECT * FROM contact';
+// db.all(sql, [], (err, rows) => {
+//     if (err) {
+//       throw err;
+//     }
+//     rows.forEach((row) => {
+//       console.log(row);
+//     });
+//   });
 
 
+//Drop table
 
+// sql = 'DROP TABLE contact';
+// db.run(sql , (err) =>{
+//     if(err) err.message;
+//     console.log("Table deleted successfully");
+// })
