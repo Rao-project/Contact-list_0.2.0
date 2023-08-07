c056c917b9e3c5185a6bae7372e1aa648287eab2
jainish.k <119989240+jaini77@users.noreply.github.com>
2023-08-07 14:55:11 +0530

    add Javascript

--- Javascript/fav.js
+++ Javascript/fav.js
@@ -1,11 +1,26 @@
-let contact = document.querySelectorAll(".navigationBtn");
-console.log(contact[0])
-contact[0].addEventListener("click" , () => {
+document.getElementsByClassName("contact")[0].addEventListener('click', function (event) 
+{
     window.location.href="index.html";
-    // console.log("hello");
-})
-// document.getElementsByClassName("contact")[0].addEventListener('click', function (event) 
-// {
-//     window.location.href="index.html";
-//     console.log("clicked");
-// }); 
+    console.log("clicked");
+}); 
+function save(){
+    var first_name = document.getElementById("F_name").value;
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
