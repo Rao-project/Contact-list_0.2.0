67e204aa5ce9e2a9ee0edb9ea46886cdecbf83d9
jainish.k <119989240+jaini77@users.noreply.github.com>
2023-08-07 18:13:43 +0530

    add  a new  updating

--- Javascript/index.js
+++ Javascript/index.js
@@ -9,3 +9,22 @@ document.getElementsByClassName("edit")[0].addEventListener('click', function (e
     window.location.href="pro.html";
     console.log("clicked");
 }); 
+  /* When the user clicks on the button, 
+    toggle between hiding and showing the dropdown content */
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+      }
+      
+      // Close the dropdown if the user clicks outside of it
+      window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          var i;
+          for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+      }
--- Javascript/pro.js
+++ Javascript/pro.js
@@ -6,3 +6,24 @@ document.getElementsByClassName("contact")[0].addEventListener('click', function
 function save(){
     var first_name = document.getElementById("F_name").value;
 }
+     /* When the user clicks on the button, 
+        toggle between hiding and showing the dropdown content */
+        function myFunction() {
+            document.getElementById("myDropdown").classList.toggle("show");
+        }
+
+        // Close the dropdown if the user clicks outside of it
+        window.onclick = function (event) {
+            if (!event.target.matches('.dropbtn')) {
+                var dropdowns = document.getElementsByClassName("dropdown-content");
+                var i;
+                for (i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
+
+
